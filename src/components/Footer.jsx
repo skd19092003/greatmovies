2deed5726dc4e47496f5fcb56ba9ee4f50162605
Sandeep kumar das <154823304+skd19092003@users.noreply.github.com>
2025-09-21 03:06:38 +0530

    Modify Footer component styles and margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 export default function Footer() {
     return (
-        <footer className="bg-dark text-light py-3 mt-4">
+        <footer className="bg-dark text-light py-3 mt-5" style={{
+            position: 'relative',
+            zIndex: 1000
+        }}>
             <div className="container text-center">
                 <p>&copy; {new Date().getFullYear()} CineVault. All rights reserved.</p>
                 <p>&copy; Made with ❤️ by <a href="https://www.linkedin.com/in/skd03/" target="_blank">SKD</a>.</p>
